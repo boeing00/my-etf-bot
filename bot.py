c32adcd5681e617034fccacedb062ed1cfa36207
boeing00 <81122476+boeing00@users.noreply.github.com>
2026-06-29 14:07:07 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 bot = telebot.TeleBot(TOKEN)
 
 portfolio = [
-    {"name": "나스닥100", "symbol": "QQQ", "buy_price": 400.0, "quantity": 10},
     {"name": "PLUS 선진국MSCI(합성H)", "symbol": "195970.KS", "buy_price": 15935, "quantity": 63},
     {"name": "kodex 200", "symbol": "069500.KS", "buy_price": 134140, "quantity": 7},
     {"name": "KODEX 미국S&P500", "symbol": "379800.KS", "buy_price": 25352, "quantity": 49}
